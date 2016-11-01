065b21b6a6454e1ff33882bd40c74f584c4c0237
Jeff Linn <linnterweb@gmail.com>
2016-11-01 11:42:21 -0500

    Fixed Mario

--- client/script.js
+++ client/script.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
         switch(parseInt(key.which,10)) {
 			// Left arrow key pressed
 			case 37:
-				$('img').animate({left: "-=50px"}, 'fast');
+				$('.mario').animate({left: "-=50px"}, 'fast');
 				break;
 			// Up Arrow Pressed
 			case 38:
-				$('img').animate({top: "-=50px"}, 'fast');
+				$('.mario').animate({top: "-=50px"}, 'fast');
 				break;
 			// Right Arrow Pressed
 			case 39:
-				$('img').animate({left: "+=50px"}, 'fast');
+				$('.mario').animate({left: "+=50px"}, 'fast');
 				break;
 			// Down Arrow Pressed
 			case 40:
-				$('img').animate({top: "+=50px"}, 'fast');
+				$('.mario').animate({top: "+=50px"}, 'fast');
 				break;
 		}
 	});

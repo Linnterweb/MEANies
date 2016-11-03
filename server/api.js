494cc1f4a9b8377c2cced0ded10b4fc94684e813
Camille Parks <chat.noir@utexas.edu>
2016-11-03 15:37:38 -0500

    boss code

--- server/api.js
+++ server/api.js
@@ -2,11 +2,12 @@ var express = require('express');
 
 var questions = require('./controllers/questions.ctrl');
 var users = require('./controllers/users.ctrl');
+var bosses = require('./controllers/bosses.ctrl');
 
 var router = express.Router();
 
 router.use('/questions', questions);
 router.use('/users', users);
-
+router.use('/bosses', bosses);
 
 module.exports = router;

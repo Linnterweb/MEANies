9064ace8c84feb8f4cbd7f646bb24f3187f9bb2d
Jeff Linn <linnterweb@gmail.com>
2016-11-04 11:14:43 -0500

    Update User Progress working

--- client/controllers.js
+++ client/controllers.js
@@ -8,7 +8,7 @@ angular.module("MEANies.controllers", [])
         //  });   
     }])
     
-    .controller('BoardController', ['$scope', '$location', 'Question', '$routeParams', 'User', function ($scope, $location, Question, $routeParams, User) {
+    .controller('BoardController', ['$scope', '$location', 'Question', '$routeParams', 'User', 'UserService', function ($scope, $location, Question, $routeParams, User, UserService) {
         // $scope.detailMode = false; // start off NOT showing details anywhere on the page
         $scope.showingDetails = false;
 
@@ -75,7 +75,8 @@ angular.module("MEANies.controllers", [])
                     var answer = prompt("What'll it be pardner?");
 
                     if (answer.toLowerCase() === (question.answer).toLowerCase()) {
-                    var user = User.get({ id: quest }, function(user) {
+                    var user = User.me(function(user) {
+                    // $scope.user = User.me();
                     var updateUser = function() {
                         user.$update(function(success) {
 
